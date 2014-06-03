99fec4cfa252e2b49bf552114a95c99587e7bbe6
Tony Edwards <tony.edwards@gmail.com>
2014-06-03 17:45:56 +0100

    0.2 - Custom pages
    Template expanded to include layouts for pages and single posts.

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,9 @@
     <aside class="right-col">
+        <?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar('Primary Sidebar') ) : ?>
+        
         <div class="widget">
-            <h3>ABOUT US</h3>
-            <p>Ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-
-            <p>Vestibulum id ligula porta felis euismod semper. Curabitur blandit tempus porttitor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-
-            <p><a href="#">MORE ABOUT US...</a></p>
+            <h3>Search</h3>
+            <?php get_search_form(); ?>
         </div>
+        <?php endif; ?>
     </aside>
---
+++ single.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+<div id="main" class="group">
+    <div id="blog" class="left-col">
+        
+        <?php if (have_posts() ) : while (have_posts() ) : the_post(); ?>
+        
+        <div class="post">
+            <h2><?php the_title(); ?></h2>
+            <div class="byline">by <?php the_author_posts_link(); ?> on <span class="date"><?php the_time('l F d, Y'); ?></span></div>
+            <?php the_content('Read More...'); ?>
+        </div>
+        
+         <div class="navi">
+            <div class="right">    
+                <?php previous_post_link(); ?> / <?php next_post_link(); ?>          
+            </div>
+        </div>      
+        
+        <?php endwhile ; else: ?>
+            <p><?php _e('No posts were found. Sorry!'); ?></p>
+        <?php endif; ?>
+
+
+    </div>
+    <?php get_sidebar(); ?>
+</div>
+
+<?php get_footer(); ?>
