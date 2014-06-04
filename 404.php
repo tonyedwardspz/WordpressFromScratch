598b0c24df8e427bc325da0e401c75665dac5bd9
Tony Edwards <tony.edwards@gmail.com>
2014-06-04 10:45:23 +0100

    0.3 - Added pages
    - Added 404 page
    - Added page-archives page

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+    <div id="main" class="group">
+        <h2>404: Page not found</h2>
+        <img class="aligncenter left" src="<?php print IMAGES; ?>/404.jpg" alt="404'd!"/>
+        <p>I am just as supprised as you are! Maybe we should just move along back to the <a href="<?php bloginfo('home'); ?>">homepage.</a></p>
+    </div>
+    <?php get_footer(); ?>
